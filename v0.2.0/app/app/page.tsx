ae4f1d68d02a842f6b17a1b8c6c8037a2cc666d5
asdtae <112775132+asdtae@users.noreply.github.com>
2025-02-15 20:08:30 +0200

    v.0.2.1
    what is the meaning of life....

---
+++ v0.2.0/app/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return(
+        <div id="test">
+            <a href="../page.tsx">Cyclesphere</a>
+            <a>App</a>
+            <a>Docs</a>
+            <a>Sign In</a>
+        </div>
+    );
+}
--- v0.2.0/app/page.tsx
+++ v0.2.0/app/page.tsx
@@ -1,7 +1,35 @@
-import Image from "next/image";
+import LikeButton from './like-button';
 
-export default function Home() {
+/*export default function Home() {
   return(
-
+      <div>
+          <h1>Hello</h1>
+          <div id="test">
+              <a>Cyclesphere</a>
+              <a href="./app/page.tsx">App</a>
+              <a>Docs</a>
+              <a>Sign In</a>
+          </div>
+      </div>
   );
+}*/
+
+function Header({ title }) {
+    return <h1>{title ? title : 'Default title'}</h1>;
+}
+
+export default function HomePage() {
+    const names = ['Ada Lovelace', 'Grace Hopper', 'Margaret Hamilton'];
+
+    return (
+        <div>
+            <Header title="Develop. Preview. Ship." />
+            <ul>
+                {names.map((name) => (
+                    <li key={name}>{name}</li>
+                ))}
+            </ul>
+            <LikeButton />
+        </div>
+    );
 }
