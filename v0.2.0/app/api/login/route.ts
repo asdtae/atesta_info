a7a2249f4b5acccc6dd5144f3cc8e886ac5509af
asdtae <112775132+asdtae@users.noreply.github.com>
2025-03-03 08:11:44 +0200

    rt

--- v0.2.0/app/api/login/route.ts
+++ v0.2.0/app/api/login/route.ts
@@ -1,8 +1,36 @@
 import { NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+import mysql from "mysql2/promise";
+
+const db = await mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "cyclesphere",
+});
 
 export async function POST(req: Request) {
     const { email, password } = await req.json();
-
+/*
+	try { 
+            const [existingUsers] = await db.execute("SELECT * FROM users WHERE email = ?", [email]);
+            if ((existingUsers as any[]).length > 0) {
+                const hashedPassword = await db.execute("SELECT * FROM users WHERE password = ?", [password]);
+                if((hashedPassword as any[]).length > 0) {
+                    const token =  email;
+                    return NextResponse.json({ success: true, message: "Login successful!", token }, { status: 201 });
+                }
+                else {
+                    return NextResponse.json({ success: false, message: "Invalid email or password. DEBUG: INVVALID PASSWORD" }, { status: 401 });
+                }
+            }
+            else {
+                return NextResponse.json({ success: false, message: "Invalid email or password. DEBUG: INVVALID EMAIL" }, { status: 401 });
+            }
+        } catch (error) {
+            return NextResponse.json({ success: false, message: "Invalid credentials." }, { status: 401 });
+        }
+*/
     if (email === "test@example.com" && password === "password123") {
         const token =  'valid-token';
         return NextResponse.json({ success: true, message: "Login successful!", token}, { status: 200 });
